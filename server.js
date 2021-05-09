f41c9c4e5c87be2a5be2fce05b49789ea2eea7c6
Tuogu <898169584@qq.com>
2021-05-09 14:42:26 +0800

    static server

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
-var port = process.argv[2]
+let http = require('http')
+let fs = require('fs')
+let url = require('url')
+let port = process.argv[2]
 
 if(!port){
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
-  var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
-  var queryString = ''
+let server = http.createServer(function(request, response){
+  let parsedUrl = url.parse(request.url, true)
+  let pathWithQuery = request.url 
+  let queryString = ''
   if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
-  var path = parsedUrl.pathname
-  var query = parsedUrl.query
-  var method = request.method
+  let path = parsedUrl.pathname
+  let query = parsedUrl.query
+  let method = request.method
 
   /******** 从这里开始看，上面可以忽略 ************/
 
